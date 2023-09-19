0cab801bb6dc005f0507576badbd1b2bfb499dd2
uday-sai-k <saisumauday@gmail.com>
2023-09-19 19:12:33 +0530

    first commit

---
+++ JavaNew.java
@@ -0,0 +1,12 @@
+class JavaNew 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+		System.out.println("I am into test repo");
+	}
+}
